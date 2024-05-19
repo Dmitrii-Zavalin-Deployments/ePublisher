c24cda3197e50c15173c97270f4790ec41e72c66
Dmitrii Zavalin's Aerospace & IT Projects <135647339+Dmitrii-Zavalin-Deployments@users.noreply.github.com>
2024-05-19 16:25:51 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 # Configuration variables
 FACEBOOK_API_KEY = os.environ.get('FACEBOOK_API_KEY')
+FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
 FACEBOOK_PAGE_ID = os.environ.get('FACEBOOK_PAGE_ID')  # Added this line for the page ID
 TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
 INSTAGRAM_API_KEY = os.environ.get('INSTAGRAM_API_KEY')
