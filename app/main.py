37fb17f34bcd8221e9390a28848e111d972a48c3
Dmitrii Zavalin's Aerospace & IT Projects <135647339+Dmitrii-Zavalin-Deployments@users.noreply.github.com>
2024-05-20 17:57:08 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from content_manager import ContentManager
 def main():
      # Initialize managers
     content_manager = ContentManager(number_of_projects=int(os.getenv('NUMBER_OF_PROJECTS')))
-    # epublisher_facebook_manager = EPublisherFacebookManager()
+    epublisher_facebook_manager = EPublisherFacebookManager()
 
     # Get Run Number
     run_number = str(content_manager.get_run_number())
@@ -66,15 +66,14 @@ def main():
     print(f"Content before hashtag:\n{content_before_hashtag}")
 
     # New: Get and print the Facebook posts
-    # facebook_posts = epublisher_facebook_manager.get_facebook_posts()
-    # print(f"Facebook posts:\n{facebook_posts}")
+    facebook_posts = epublisher_facebook_manager.get_facebook_posts()
+    print(f"Facebook posts:\n{facebook_posts}")
 
     # Searching for posts to delete
-
-    # message_ids = epublisher_facebook_manager.print_message_before_hashtag(facebook_posts, content_before_hashtag)
+    message_ids = epublisher_facebook_manager.print_message_before_hashtag(facebook_posts, content_before_hashtag)
 
     # Post in Facebook
-    # epublisher_facebook_manager.post_to_facebook(image_path, text_content)
+    epublisher_facebook_manager.post_to_facebook(image_path, text_content)
 
 if __name__ == "__main__":
     main()
