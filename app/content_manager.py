8d732f0d2e4bf50eb424e120268bef846be1fa40
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2024-10-22 01:38:55 -0300

    Testing

--- app/content_manager.py
+++ app/content_manager.py
@@ -110,7 +110,7 @@ class ContentManager:
         file_path = f'content/text/{file_index}.txt'
         
         # Initialize the GPT-4All model
-        model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
+        model = GPT4All("orca-mini-7b")
         
         # Read existing file content
         try:
